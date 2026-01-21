baa4355016a719dade86af12bd9af210b9563b4e
lam584 <ls20@vip.qq.com>
2026-01-21 17:15:06 +0800

    删除若干历史遗留代码

--- my-vite-app/src/icons.ts
+++
@@ -1,5 +0,0 @@
-// src/icons.ts
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCoffee, faUser } from '@fortawesome/free-solid-svg-icons';
-// …根据需要再从 free-regular/free-brands 引入
-library.add(faCoffee, faUser);
